1e26bf273e9b7592d91ff13a6bfaa4ddd33a9b4c
JPRR44 <juanp.robles@hotmail.com>
2021-05-03 09:02:53 -0500

    Cambio de implementación

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-// Juan Pablo Ramos Robles
+// Juan Pablo Ramos Robles 715592
+
+// Manda a llamar a NLU
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
+
+// Instancia de NAtual Language
 
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2020-08-01',
   authenticator: new IamAuthenticator({
+    // Mandamos llamar las variabls de entorno
     apikey: process.env.apikey,
   }),
   serviceUrl: process.env.url,
 });
-const response = {
-  statusCode: 200,
-  body: JSON.stringify('Hello from Lambda!'),
-};
+// Exportamos el handler
 exports.handler = async (event) => {
-
+  // Tiene limite de dos ya que es lo que nos pide
   const analyzeParams = {
-    'text': event.historial_clinico
+    'text': event.historial,
+    'features': {
+      'entities': {
+        'emotion': true,
+        'sentiment': true,
+        'limit': 2
+      },
+      'keywords': {
+        'emotion': true,
+        'sentiment': true,
+        'limit': 2
+      }
+    }
   };
 
-  naturalLanguageUnderstanding.analyze(analyzeParams)
-    .then(analysisResults => {
-      return JSON.stringify(analysisResults, null, 2)
-    })
-    .catch(err => {
-      console.log('error:', err);
-      return err;
-    });
+  // Resultado del analizer
+  const res = await naturalLanguageUnderstanding.analyze(analyzeParams)
+  return res;
 };
