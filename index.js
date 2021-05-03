3dd734eea6f83d803de3a5885a8f1e91bea9ebef
JPRR44 <juanp.robles@hotmail.com>
2021-05-03 16:22:39 -0500

    Se añadió implementación para mapear los datos como pide la salida

--- index.js
+++ index.js
@@ -33,7 +33,77 @@ exports.handler = async (event) => {
     }
   };
 
-  // Resultado del analizer
+  // Resultado que se va a analizar con NLU
   const res = await naturalLanguageUnderstanding.analyze(analyzeParams)
-  return res;
+
+  // Arreglo utilizado para las palabras claves
+  let palabrasClave = []
+  for (let i = 0; i < res.result.keywords.length; i++)  palabrasClave.push(res.result.keywords[i].text)
+  //Arreglo usado paral as entidades
+  let entidadesGroup = []
+  for (let i = 0; i < res.result.entities.length; i++) entidadesGroup.push(res.result.entities[i].text)
+
+  //Función para sacar la más grande
+  function getMaxEmotion(emotions) {
+    var top;
+    var topValue;
+    for (let emotion in emotions) {
+      if (topValue == null) {
+        top = emotion
+        topValue = emotions[emotion]
+
+      } else if (topValue < emotions[emotion]) {
+        topValue = emotions[emotion]
+        top = emotion
+      }
+    }
+    // Este es tu emoción máxima
+    return top;
+  }
+
+  // Con esto sacamos las palabras claves pero las descendentes
+  let palabrasClaveDesc = {}
+  // Recorremos result para llenar el objeto
+  for (let i = 0; i < res.result.keywords.length; i++) {
+    let nuevaClave = {
+      "sentimiento": res.result.keywords[i].sentiment.label,
+      "relevancia": res.result.keywords[i].relevance,
+      "repeticiones": res.result.keywords[i].count,
+      "emocion": getMaxEmotion(res.result.keywords[i].emotion)
+    }
+    // Se crea la nuea palabra clave
+    palabrasClaveDesc[res.result.keywords[i].text] = nuevaClave
+  }
+
+  // Con esto sacamos las entidades 
+  let entidadesDesc = {}
+  //Recorremos result para llenar el objeto
+  for (let i = 0; i < res.result.entities.length; i++) {
+    let nuevaEntidad = {
+      "tipo": res.result.entities[i].type,
+      "sentimiento": res.result.entities[i].sentiment.label,
+      "relevancia": res.result.entities[i].relevance,
+      "emocion": getMaxEmotion(res.result.entities[i].emotion),
+      "repeticiones": res.result.entities[i].count,
+      "porcentaje_confianza": res.result.entities[i].confidence
+    }
+    // Se crea la nueva entidad
+    entidadesDesc[res.result.keywords[i].text] = nuevaEntidad
+  }
+
+  // Resultado que se necesira sacar en JSON
+  FinalResult = {
+    "lenguage_texto": res.result.language,
+    "palabras_clave": palabrasClave,
+    "entidades": entidadesGroup,
+    "palabras_clave_desc": palabrasClaveDesc,
+    "entidades_desc": entidadesDesc
+  }
+
+
+  return FinalResult;
+
+
 };
+
+
